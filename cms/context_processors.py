5d3b29bfd98ea77245cbb5f1ca9d7cac1c66c616
Jacob Rief <jacob.rief@gmail.com>
2019-11-20 13:16:53 +0100

    do not rename function names

--- cms/context_processors.py
+++ cms/context_processors.py
@@ -22,10 +22,10 @@ def cms_settings(request):
     # Now use lazy() to avoid getting the menu renderer
     # up until the point is needed.
     # lazy() does not memoize results, is why lru_cache is needed.
-    _get_menu_renderer = lazy(_get_menu_renderer, MenuRenderer)
+    get_menu_renderer = lazy(_get_menu_renderer, MenuRenderer)
 
     return {
-        'cms_menu_renderer': _get_menu_renderer(),
+        'cms_menu_renderer': get_menu_renderer(),
         'CMS_MEDIA_URL': get_cms_setting('MEDIA_URL'),
         'CMS_TEMPLATE': lambda: get_page_template_from_request(request),
     }

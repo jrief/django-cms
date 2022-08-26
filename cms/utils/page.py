100b9d3017745a22b733b5103e10d26cd66385b6
Jacob Rief <jacob.rief@gmail.com>
2022-08-26 08:55:07 +0200

    Revert "Prevent viewing pages outside their published dates range"
    This reverts commit 498239f4b19eb54a016fbf426582f7fe1847de28.

--- cms/utils/page.py
+++ cms/utils/page.py
@@ -138,7 +138,7 @@ def get_page_from_request(request, use_path=None, clean_path=None):
 
     The page slug can then be resolved to a Page model object
     """
-    from cms.utils.page_permissions import user_can_view_page_draft, user_can_publish_page
+    from cms.utils.page_permissions import user_can_view_page_draft
 
     if not bool(use_path) and hasattr(request, '_current_page_cache'):
         # The following is set by CurrentPageMiddleware
@@ -171,7 +171,7 @@ def get_page_from_request(request, use_path=None, clean_path=None):
     # In this case the selected page is not reachable
     if page and not draft:
         now = timezone.now()
-        if not user_can_publish_page(request.user, page) and (
+        if (
             page.publication_date and page.publication_date > now
             or page.publication_end_date and page.publication_end_date < now
             or page.get_ancestor_pages().filter(

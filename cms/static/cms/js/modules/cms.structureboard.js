jrief/django-cms

commit d3fbc8ab7b0f27c847998e813480ad6526c550bd

fix: Scan plugin data after structure mode Xhr load

cms/static/cms/js/modules/cms.structureboard.js
@@ -388,25 +388,20 @@ class StructureBoard {
 
                 CMS.settings.states = Helpers.getSettings().states;
 
-                var bodyRegex = /<body[\S\s]*?>([\S\s]*)<\/body>/gi;
-                var body = $(bodyRegex.exec(contentMarkup)[1]);
+                const bodyRegex = /<body[\S\s]*?>([\S\s]*)<\/body>/gi;
+                const body = document.createElement("div");  // Switch to plain JS due to problem with $(body)
+                body.innerHTML = bodyRegex.exec(contentMarkup)[1];
 
-                var structure = body.find('.cms-structure-content');
-                var toolbar = body.find('.cms-toolbar');
-                var scripts = body.filter(function() {
-                    var elem = $(this);
-
-                    return elem.is('[type="text/cms-template"]'); // cms scripts
-                });
-                const pluginIds = this.getIds(body.find('.cms-draggable'));
-                const pluginDataSource = body.filter('script[data-cms]').toArray()
-                    .map(script => script.textContent || '').join();
+                const structure = $(body.querySelector('.cms-structure-content'));
+                const toolbar = $(body.querySelector('.cms-toolbar'));
+                const scripts = $(body.querySelector('[type="text/cms-template"]')); // cms scripts
+                const pluginIds = this.getIds($(body.querySelectorAll('.cms-draggable')));
                 const pluginData = StructureBoard._getPluginDataFromMarkup(
-                    pluginDataSource,
+                    body,
                     pluginIds
                 );
 
-                Plugin._updateRegistry(pluginData.map(([, data]) => data));
+                Plugin._updateRegistry(pluginData);
 
                 CMS.API.Toolbar._refreshMarkup(toolbar);
 
@@ -1477,21 +1472,19 @@ class StructureBoard {
      *
      * @method _getPluginDataFromMarkup
      * @private
-     * @param {String} markup
+     * @param {Node} body
      * @param {Array<Number | String>} pluginIds
      * @returns {Array<[String, Object]>}
      */
-    static _getPluginDataFromMarkup(markup, pluginIds) {
+    static _getPluginDataFromMarkup(body, pluginIds) {
         return compact(
             pluginIds.map(pluginId => {
-                // oh boy
-                const regex = new RegExp(`CMS._plugins.push\\((\\["cms\-plugin\-${pluginId}",[\\s\\S]*?\\])\\)`, 'g');
-                const matches = regex.exec(markup);
+                const pluginData = body.querySelector(`#cms-plugin-${pluginId}`);
                 let settings;
 
-                if (matches) {
+                if (pluginData) {
                     try {
-                        settings = JSON.parse(matches[1]);
+                        settings = JSON.parse(pluginData.textContent);
                     } catch (e) {
                         settings = false;
                     }

commit e874569c7ed753aa7a01d08782681561ff0e7512

Fix regression introduced in d3fbc8a: All plugins can be added

cms/static/cms/js/modules/cms.structureboard.js
@@ -401,7 +401,7 @@ class StructureBoard {
 
                 const structure = $(body.querySelector('.cms-structure-content'));
                 const toolbar = $(body.querySelector('.cms-toolbar'));
-                const scripts = $(body.querySelector('[type="text/cms-template"]')); // cms scripts
+                const scripts = $(body.querySelectorAll('[type="text/cms-template"]')); // cms scripts
                 const pluginIds = this.getIds($(body.querySelectorAll('.cms-draggable')));
                 const pluginData = StructureBoard._getPluginDataFromMarkup(
                     body,